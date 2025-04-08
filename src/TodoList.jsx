2904fee40a1c66e344193d6513ac449e6947498c
Girma Ebssa <girmaye0@gmail.com>
2025-04-08 10:35:04 -0400

    basic hooks, events & handlers, and updating states

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,22 +1,12 @@
 import TodoListItem from "./TodoListItem";
 
-function TodoList() {
-  const todos = [
-    // Corrected variable name to 'todos'
-    { id: 1, title: "Reading week-03 module" },
-    { id: 2, title: "Attending Mentor sessions" },
-    { id: 3, title: "Props and State" },
-    { id: 4, title: "Starting week-03 module" },
-  ];
-
+function TodoList({ todoList }) {
   return (
-    <div>
-      <ul>
-        {todos.map((todo) => (
-          <TodoListItem todo={todo} key={todo.id} /> // Corrected key and self-closing tag
-        ))}
-      </ul>
-    </div>
+    <ul>
+      {todoList.map((todo) => (
+        <TodoListItem key={todo.id} todo={todo} />
+      ))}
+    </ul>
   );
 }
 
